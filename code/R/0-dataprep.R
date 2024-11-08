2d110226d2e4ccdd50bff72788b6c5b9eafb41e3
Gregoery J. Matthews <gregorymatthews@Gregorys-MacBook-Pro.local>
2024-11-08 15:53:36 -0600

    updste

--- code/R/0-dataprep.R
+++ code/R/0-dataprep.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 dattemp <- list()
 for (i in 1999:2023){print(i)
-temp <- read.csv(paste0("./rawdata/play_by_play_",i,".csv"))
+temp <- read.csv(paste0("/Users/gregorymatthews/Dropbox/function_clusgteringgit/rawdata/play_by_play_",i,".csv"))
 temp <- temp %>% select(game_id, home_wp,game_seconds_remaining, qtr)
 temp$season <- i
 dattemp[[i-1998]] <- temp
